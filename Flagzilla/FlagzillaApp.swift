3fa365b358943973dcdfbb2f97441b8855cac2df
njansari <nayanjansari@gmail.com>
2021-08-27 11:49:33 +0100

    Initial Commit

---
+++ Flagzilla/FlagzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagzillaApp.swift
+//  Flagzilla
+//
+//  Created by Nayan Jansari on 27/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct FlagzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
